a569422fa75d8ab9fc4fb7e272fc5b78fd8f0cf6
Miguel Delacruz <delacruzsmiguel@Gmail.com>
2018-02-07 14:27:35 -1000

    changes 2:27pm

--- js/pizza.js
+++ js/pizza.js
@@ -4,15 +4,92 @@
 * Each object will have two nested objects ex.(pizza & toppings)
 * Keys with more than one value should be an array of strings ex. Veggies
 */
+var order11821 = {
+	CustomerName:"James Franko",
+	Address:"2155 E Lake Dr",
+	City:"Seattle",
+	State:"WA",
+	ZipCode:"11310",
+	Pizza:"classic marinara",
+  	Crust:"thin",
+  	PizzaSauce:"robust marinara",
+  	Cheese:"regular",
+  	Toppings:
+    Veggies:"olives","green peppers",
+    Meats:"pepperoni", "ham", "spinach",
+	SideItems:"breadsticks", "bbq wings 6 piece",
+	Sauces:"ranch",
+	SpecialInstructions:"please cut into squares",
+	Payment Type:"cash",
+	Store Id:"112",
+	 orderTime:"20 minutes"
+}
 
+var order11372 = {
+	Customer Name:"Justin Bieber",
+	Address:"1801 W Rodeo Dr",
+	City:"Beverly Hills",
+	State:"CA",
+	ZipCode:"91833",
+	Pizza:
+  	Crust:"stuffed crust",
+ 	Pizza Sauce:"creamy garlic alfredo",
+  	Cheese:"double",
+  	Toppings:
+    Veggies:"onion, anchovies","mushrooms",
+    Meats:"chicken","bacon",
+	Side Items:"double fudge brownie dessert",
+	Sauces:"bbq","garlic sauce",
+	Special Instructions:"call 815 at the gate",
+	Payment Type:"credit card",
+	Store Id:"382",
+}
 
 /*
 * Create a new variable named currentOrders
 * currentOrders should be single object containing both orders from above
 */
+var currentOrders = {
+CustomerName:"James Franko",
+	Address:"2155 E Lake Dr",
+	City:"Seattle",
+	State:"WA",
+	ZipCode:"11310",
+	Pizza:"classic marinara"
+  	Crust: "thin",
+  	PizzaSauce:"robust marinara",
+  	Cheese:"regular",
+  	Toppings:
+    Veggies:"olives", "green peppers",
+    Meats:"pepperoni", "ham", "spinach",
+	SideItems:"breadsticks", "bbq wings 6 piece",
+	Sauces:"ranch",
+	SpecialInstructions:"please cut into squares",
+	Payment Type:"cash",
+	Store Id:"112",
+	orderTime:"20 minutes"
 
+	Customer Name:"Justin Bieber",
+	Address:"1801 W Rodeo Dr",
+	City:"Beverly Hills",
+	State:"CA",
+	ZipCode:"91833",
+	Pizza:
+  	Crust:"stuffed crust",
+ 	Pizza Sauce:"creamy garlic alfredo",
+  	Cheese:"double",
+  	Toppings:
+    Veggies:"onion, anchovies","mushrooms",
+    Meats:"chicken","bacon",
+	Side Items:"double fudge brownie dessert",
+	Sauces:"bbq","garlic sauce",
+	Special Instructions:"call 815 at the gate",
+	Payment Type:"credit card",
+	Store Id:"382",
+}
 /*
 * Make the changes to the currentOrders object based on the ReadMe
 * Run your file with node pizza.js
 * Console.log(currentOrders) to verify changes have been made
 */
+Console.log(currentOrders)
